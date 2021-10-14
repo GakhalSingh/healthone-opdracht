1e591840c8dc593f1a9f7f25174ab34ea1055d46
Gurjinder <70706370+GakhalSingh@users.noreply.github.com>
2021-10-14 12:27:18 +0200

    Home Pagina geupdate, Google maps toegevoegd

--- Templates/contact.php
+++ Templates/contact.php
@@ -15,11 +15,6 @@
             Adres: zuidhoornseweg 6a, 2635DJ Den Hoorn <br>
             E-Mail: info@healthone.com <br>
             <hr>
-            <h3>Locatie:</h3>
-            <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2456.639189021884!2d4.33007681606151!3d51.99522967971829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b44d61a108cf%3A0x9da6361d0a36ebd1!2sZuidhoornseweg%206A%2C%202625%20DV%20Den%20Hoorn!5e0!3m2!1snl!2snl!4v1633693263063!5m2!1snl!2snl" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div>
-            <hr>
             <h5>Contact opnemen</h5>
             <form>
                 <div class="form-group">
--- Templates/home.php
+++ Templates/home.php
@@ -11,7 +11,7 @@
             include_once ('defaults/pictures.php');
 
             ?>
-            <h4>Sportcenter HealthOne</h4>
+            <h3>Sportcenter HealthOne</h3>
             Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
             Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
             Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
@@ -26,6 +26,13 @@
             <a class="list-group-item list-group-item-action">Zaterdag : 08:00 - 13:00 </a>
             <a class="list-group-item list-group-item-action">Zondag   : 08:00 - 13:00 </a>
             </ul>
+            <hr>
+            
+            <h4>Locatie:</h4>
+            <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2456.639189021884!2d4.33007681606151!3d51.99522967971829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b44d61a108cf%3A0x9da6361d0a36ebd1!2sZuidhoornseweg%206A%2C%202625%20DV%20Den%20Hoorn!5e0!3m2!1snl!2snl!4v1633693263063!5m2!1snl!2snl" width="900" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+
             <hr>
             <?php
             include_once ('defaults/footer.php');
