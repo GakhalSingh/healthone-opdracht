0999f8e5560f315368ac0a96da62f4ad556077a7
Gurjinder <70706370+GakhalSingh@users.noreply.github.com>
2021-10-14 09:31:37 +0200

    FETCH

--- Classes/Category.php
+++ Classes/Category.php
@@ -12,3 +12,10 @@ class Category
         settype($this->id, 'integer');
     }
 }
+
+$selectQuery = "SELECT * FROM users WHERE id = :id";
+$query = $pdo->prepare($selectQuery);
+$id = filter_id(type:INPUT_GET,variable_name:"id",filter:FILTER_SANITIZE_NUMBER_INT);
+$query->bindParam("id", $id);
+$query->execute();
+$result = $query->fetch(fetch_style:PDO,:FETCH_ASSOC);
